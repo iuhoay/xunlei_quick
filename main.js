2c9a40d5e339b0ce8e9ab305fd5b85ed8586552f
iuhoay <iuhoay@gmail.com>
2012-11-24 11:30:42 +0800

    use api to open tab

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
     "title": "使用迅雷离线下载",
     "contexts": ["link"],
     "onclick": function(info, tab) {
-      window.open("http://lixian.vip.xunlei.com/lixian_login.html?furl=" + info.linkUrl);
+      chrome.tabs.create({
+        url: "http://lixian.vip.xunlei.com/lixian_login.html?furl=" + info.linkUrl
+      });
     }
   });
 })();
