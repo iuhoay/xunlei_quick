4d52a4413cc32f0b8ad17ca5f4de198d14b6873c
iuhoay <iuhoay@gmail.com>
2013-12-17 18:33:50 +0800

    resolved #1 对选中文字链接的右键支持

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 ;(function() {
   chrome.contextMenus.create({
     "title": "使用迅雷离线下载",
-    "contexts": ["link"],
+    "contexts": ["link", "selection"],
     "onclick": function(info, tab) {
       chrome.tabs.create({
         url: "http://lixian.vip.xunlei.com/lixian_login.html?furl=" + info.linkUrl
