17cf36c427978444ae21a86e2d7b780d8e09b0ac
iuhoay <iuhoay@gmail.com>
2014-01-01 00:25:26 +0800

    fix 选中文字链接支持问题

--- main.js
+++ main.js
@@ -3,9 +3,20 @@
     "title": "使用迅雷离线下载",
     "contexts": ["link", "selection"],
     "onclick": function(info, tab) {
-      chrome.tabs.create({
-        url: "http://lixian.vip.xunlei.com/lixian_login.html?furl=" + info.linkUrl
-      });
+      var link = get_link(info);
+      if (link) {
+        chrome.tabs.create({
+          url: "http://lixian.vip.xunlei.com/lixian_login.html?furl=" + link
+        });
+      }
     }
   });
+
+  function get_link(info) {
+    if (info.selectionText) {
+      return info.selectionText;
+    } else if (info.linkUrl) {
+      return info.linkUrl;
+    }
+  }
 })();
